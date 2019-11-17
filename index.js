05c2d01ef7c6548084f77f97b6fceeba66059f95
Rodion <rodionantonichev@gmail.com>
2019-11-17 15:37:35 +0200

    add cors remove body-parser

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const bodyParser =require("body-parser");
+const cors = require("cors");
 const todoRoutes = require("./routes/api/todo");
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log(`Connected to DB ${process.env.DB_URL}`))
   .catch(error => console.log(error));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:8081");
-  next();
-});
 
 todoRoutes(app);
 
